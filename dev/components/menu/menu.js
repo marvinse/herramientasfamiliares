034943fe577ee35860b94ddfffd1f8691ca1718e
Marvin Solano <marsolan@crsjopgp4311.global.publicisgroupe.net>
2017-07-08 23:37:17 -0600

    First components created

---
+++ dev/components/menu/menu.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.menu = (function () {
+    
+    var init = function (element) {
+        console.log('APP.menu');
+    };
+
+    return {
+        init: init
+    };
+
+}());
